5eba8d1ed906e866c51f806fcfa557fe70f48a87
chenjiangsong <274815408@qq.com>
2016-09-19 22:20:48 +0800

    commit

---
+++ src/components/v-codemirror/alias.js
@@ -0,0 +1,11 @@
+import CodeMirror from 'codemirror/lib/codemirror.js';
+require('codemirror/lib/codemirror.css');
+require('codemirror/addon/lint/lint.css');
+require('codemirror/mode/javascript/javascript.js');
+require('codemirror/mode/css/css.js');
+require('jsonlint');
+require('codemirror/addon/lint/lint.js');
+require('codemirror/addon/lint/json-lint.js');
+require('codemirror/addon/selection/active-line.js');
+
+export default CodeMirror;
---
+++ src/components/v-codemirror/index.vue
@@ -0,0 +1,27 @@
+<template>
+    <textarea v-el:editor></textarea>
+</template>
+<script>
+    import CodeMirror from './alias';
+    export default {
+        ready () {
+            const dom = this.$els.editor
+            this.initEditor(dom)
+        },
+        methods: {
+            initEditor(dom, readOnly) {
+                const editor = CodeMirror.fromTextArea(dom, {
+                    lineNumbers: true,
+                    mode: 'application/json',
+                    lint: true,
+                    gutters: ['CodeMirror-lint-markers'],
+                    readOnly: readOnly ? 'nocursor' : false,
+                    tabSize: 2,
+                    styleActiveLine: true
+                })
+            }
+        }
+    };
+</script>
+<style>
+</style>
